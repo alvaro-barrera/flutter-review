7f9b7e33009e9ddc479504f66b31967ac9fb4e8e
alvaro <alvaro_bv@hotmail.com>
2020-11-05 15:06:47 -0500

    Botón navegar y snackbar

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:platzi_trips_app/button_purple.dart';
 
 class DescriptionPlace extends StatelessWidget {
 
@@ -96,9 +97,11 @@ class DescriptionPlace extends StatelessWidget {
 
 
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget> [
         title_star,
-        description
+        description,
+        ButtonPurple("Navegar")
       ],
     );
     
